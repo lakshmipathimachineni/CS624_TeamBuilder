ad7977f47511bec27db43d0511741df85ef2af41
lakshmipathimachineni <122321993+lakshmipathimachineni@users.noreply.github.com>
2023-12-07 03:17:38 +0000

    Final code

--- Team/myteam/StudentInfoPage.js
+++ Team/myteam/StudentInfoPage.js
@@ -1,4 +1,4 @@
-import { Alert, ScrollView, Text, TextInput, View, Button, StyleSheet } from 'react-native';
+import { Alert, ScrollView, Text, TextInput, View, Button, FlatList, StyleSheet } from 'react-native';
 import React, { useState } from 'react';
 import { useAppContext } from './AppContext';
 
@@ -52,65 +52,40 @@ const StudentInfoPage = ({ route }) => {
   const formTeams = () => {
     const teams = [];
     const studentsAssigned = new Set();
-
-    const studentsByCountry = {};
+  
     state.students.forEach((student) => {
-      if (!studentsByCountry[student.country]) {
-        studentsByCountry[student.country] = [];
+      if (!studentsAssigned.has(student.id)) {
+        const team = [];
+        team.push(student);
+        studentsAssigned.add(student.id);
+  
+        const otherStudents = state.students.filter(
+          (s) => !studentsAssigned.has(s.id)
+        );
+  
+        otherStudents.forEach((otherStudent) => {
+          if (team.length < 2 && otherStudent.country !== student.country) {
+            team.push(otherStudent);
+            studentsAssigned.add(otherStudent.id);
+          }
+        });
+  
+        teams.push(team);
       }
-      studentsByCountry[student.country].push(student);
     });
-
-    Object.values(studentsByCountry).forEach((countryStudents) => {
-      const team = [];
-      const experiencesUsed = new Set();
-
-      countryStudents.sort((a, b) => b.experience - a.experience);
-
-      countryStudents.forEach((student) => {
-        if (
-          !studentsAssigned.has(student.id) &&
-          team.length < 4 &&
-          !experiencesUsed.has(student.experience)
-        ) {
-          team.push(student);
-          studentsAssigned.add(student.id);
-          experiencesUsed.add(student.experience);
-        }
-
-        if (team.length >= 2 && (team.length === 4 || experiencesUsed.size === 2)) {
-          teams.push([...team]);
-          team.length = 0;
-          experiencesUsed.clear();
-        }
-      });
-    });
-
-    const remainingStudents = state.students.filter((student) => !studentsAssigned.has(student.id));
-
-    const shuffledRemainingStudents = shuffleArray(remainingStudents);
-    while (shuffledRemainingStudents.length > 0) {
-      for (let i = 0; i < teams.length && shuffledRemainingStudents.length > 0; i++) {
-        teams[i].push(shuffledRemainingStudents.pop());
-      }
-    }
-
+  
     setFormedTeams(teams);
     dispatch({ type: 'FORM_TEAMS', payload: teams });
-
+  
     Alert.alert('Team Formation Completed', 'The teams have been successfully formed!');
   };
 
-  const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-
   const getTeamDetails = () => {
-    setDisplayedTeams(true);
+    if (formedTeams.length > 0) {
+      setDisplayedTeams(true);
+    } else {
+      Alert.alert('Teams Not Formed', 'Please form teams before getting team details.');
+    }
   };
 
   const getStudentsList = () => {
@@ -118,91 +93,93 @@ const StudentInfoPage = ({ route }) => {
   };
 
   return (
-    <ScrollView horizontal>
-      <View style={styles.container}>
-        <View style={styles.fieldRow}>
-          <Text style={styles.fieldLabel}>Name</Text>
-          <TextInput
-            style={styles.input}
-            value={studentInfo.name}
-            onChangeText={(text) => handleInputChange('name', text)}
-            placeholder="Name"
-          />
-        </View>
-        <View style={styles.fieldRow}>
-          <Text style={styles.fieldLabel}>Email</Text>
-          <TextInput
-            style={styles.input}
-            value={studentInfo.email}
-            onChangeText={(text) => handleInputChange('email', text)}
-            placeholder="Email"
-          />
-        </View>
-        <View style={styles.fieldRow}>
-          <Text style={styles.fieldLabel}>ID</Text>
-          <TextInput
-            style={styles.input}
-            value={studentInfo.id}
-            onChangeText={(text) => handleInputChange('id', text)}
-            placeholder="ID"
-          />
-        </View>
-        <View style={styles.fieldRow}>
-          <Text style={styles.fieldLabel}>Country</Text>
-          <TextInput
-            style={styles.input}
-            value={studentInfo.country}
-            onChangeText={(text) => handleInputChange('country', text)}
-            placeholder="Country"
-          />
-        </View>
-        <View style={styles.fieldRow}>
-          <Text style={styles.fieldLabel}>Experience</Text>
-          <TextInput
-            style={styles.input}
-            value={studentInfo.experience}
-            onChangeText={(text) => handleInputChange('experience', text)}
-            placeholder="Experience"
-          />
-        </View>
-        <View style={styles.buttonRow}>
-          <Button title="Add Student Info" onPress={addStudentInfo} style={styles.button} />
-          <Button title="Team Formation" onPress={formTeams} style={styles.button} />
-          <Button title="Get Team Details" onPress={getTeamDetails} style={styles.button} />
-          <Button title="Students List" onPress={getStudentsList} style={styles.button} />
-        </View>
-
-        {displayStudentsList && (
-          <View>
-            <Text style={styles.teamHeader}>Students List:</Text>
-            {state.students.map((student, index) => (
-              <View key={index} style={styles.teamContainer}>
-                <Text style={styles.teamText}>{`Name: ${student.name}`}</Text>
-                <Text style={styles.teamText}>{`Email: ${student.email}`}</Text>
-                <Text style={styles.teamText}>{`ID: ${student.id}`}</Text>
-                <Text style={styles.teamText}>{`Country: ${student.country}`}</Text>
-                <Text style={styles.teamText}>{`Experience: ${student.experience}`}</Text>
-              </View>
-            ))}
+    <ScrollView>
+      <ScrollView horizontal>
+        <View style={styles.container}>
+          <View style={styles.fieldRow}>
+            <Text style={styles.fieldLabel}>Name</Text>
+            <TextInput
+              style={styles.input}
+              value={studentInfo.name}
+              onChangeText={(text) => handleInputChange('name', text)}
+              placeholder="Name"
+            />
           </View>
-        )}
+          <View style={styles.fieldRow}>
+            <Text style={styles.fieldLabel}>Email</Text>
+            <TextInput
+              style={styles.input}
+              value={studentInfo.email}
+              onChangeText={(text) => handleInputChange('email', text)}
+              placeholder="Email"
+            />
+          </View>
+          <View style={styles.fieldRow}>
+            <Text style={styles.fieldLabel}>ID</Text>
+            <TextInput
+              style={styles.input}
+              value={studentInfo.id}
+              onChangeText={(text) => handleInputChange('id', text)}
+              placeholder="ID"
+            />
+          </View>
+          <View style={styles.fieldRow}>
+            <Text style={styles.fieldLabel}>Country</Text>
+            <TextInput
+              style={styles.input}
+              value={studentInfo.country}
+              onChangeText={(text) => handleInputChange('country', text)}
+              placeholder="Country"
+            />
+          </View>
+          <View style={styles.fieldRow}>
+            <Text style={styles.fieldLabel}>Experience</Text>
+            <TextInput
+              style={styles.input}
+              value={studentInfo.experience}
+              onChangeText={(text) => handleInputChange('experience', text)}
+              placeholder="Experience"
+            />
+          </View>
+          <View style={styles.buttonRow}>
+            <Button title="Add Student Info" onPress={addStudentInfo} style={styles.button} />
+            <Button title="Team Formation" onPress={formTeams} style={styles.button} />
+            <Button title="Get Team Details" onPress={getTeamDetails} style={styles.button} />
+            <Button title="Students List" onPress={getStudentsList} style={styles.button} />
+          </View>
 
-        <View>
-          <Text style={styles.teamHeader}>Formed Teams:</Text>
-          {displayedTeams &&
-            formedTeams.map((team, teamIndex) => (
-              <View key={teamIndex} style={styles.teamContainer}>
-                <Text style={styles.teamText}>{`Team ${teamIndex + 1}:`}</Text>
-                {team.map((teamMember, memberIndex) => (
-                  <Text
-                    key={memberIndex}
-                    style={styles.teamText}
-                  >{`${teamMember.name} (${teamMember.country})`}</Text>
-                ))}
-              </View>
-            ))}
+          {displayStudentsList && (
+            <View>
+              <Text style={styles.teamHeader}>Students List:</Text>
+              {state.students.map((student, index) => (
+                <View key={index} style={styles.teamContainer}>
+                  <Text style={styles.teamText}>{`Name: ${student.name}`}</Text>
+                  <Text style={styles.teamText}>{`Email: ${student.email}`}</Text>
+                  <Text style={styles.teamText}>{`ID: ${student.id}`}</Text>
+                  <Text style={styles.teamText}>{`Country: ${student.country}`}</Text>
+                  <Text style={styles.teamText}>{`Experience: ${student.experience}`}</Text>
+                </View>
+              ))}
+            </View>
+          )}
+
+          <View>
+            <Text style={styles.teamHeader}>Formed Teams:</Text>
+            {displayedTeams &&
+              formedTeams.map((team, teamIndex) => (
+                <View key={teamIndex} style={styles.teamContainer}>
+                  <Text style={styles.teamText}>{`Team ${teamIndex + 1}:`}</Text>
+                  {team.map((teamMember, memberIndex) => (
+                    <Text
+                      key={memberIndex}
+                      style={styles.teamText}
+                    >{`${teamMember.name} (${teamMember.country})`}</Text>
+                  ))}
+                </View>
+              ))}
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </ScrollView>
   );
 };
